0b4e5557786e61267ec6e69350ab8e0920e336d9
Edin Alibegović <edinalibegovic001@gmail.com>
2026-08-07 12:03:51 +0200

    feat: implemented AsistentPocetnaView functionality

--- FakultetApp/Views/AsistentView/AsistentPocetnaView.xaml.cs
+++ FakultetApp/Views/AsistentView/AsistentPocetnaView.xaml.cs
@@ -1,16 +1,51 @@
 ﻿using Fakultet.Core.Modeli;
+using Fakultet.Servisi.IServis.FakultetskiProcesi;
 using System.Windows.Controls;
 
 namespace FakultetApp.Views.AsistentView
 {
-    /// <summary>
-    /// Interaction logic for AsistentPocetnaView.xaml
-    /// </summary>
     public partial class AsistentPocetnaView : UserControl
     {
-        public AsistentPocetnaView(Asistent asistent)
+        private readonly AsistentPredmetServis _asistentPredmetServis;
+        private readonly StudentPredmetServis _studentPredmetServis;
+        private readonly Asistent _ulogovaniAsistent;
+
+        public AsistentPocetnaView(AsistentPredmetServis apServis, StudentPredmetServis spServis, Asistent asistent)
         {
             InitializeComponent();
+
+            _asistentPredmetServis = apServis;
+            _studentPredmetServis = spServis;
+            _ulogovaniAsistent = asistent;
+
+            UcitajPodatke();
+        }
+
+        private void UcitajPodatke()
+        {
+            txtPozdrav.Text = $"Dobro došli, {_ulogovaniAsistent.Ime} {_ulogovaniAsistent.Prezime}!";
+            txtDatum.Text = $"Danas je {DateTime.Now:dd.MM.yyyy.}";
+
+            var predmeti = _asistentPredmetServis.GetPredmetiByAsistent(_ulogovaniAsistent.Id);
+            txtBrojPredmeta.Text = predmeti.Count.ToString();
+
+            int ukupanBrojStudenata = 0;
+            int ukupnoPolozenih = 0;
+
+            foreach (var predmet in predmeti)
+            {
+                var studentiNaPredmetu = _studentPredmetServis.GetStudentiByPredmet(predmet.Id, "");
+                ukupanBrojStudenata += studentiNaPredmetu.Count;
+                ukupnoPolozenih += studentiNaPredmetu.Count(s => s.Polozio);
+            }
+
+            txtBrojStudenata.Text = ukupanBrojStudenata.ToString();
+
+            if (ukupanBrojStudenata > 0)
+            {
+                double prolaznost = (double)ukupnoPolozenih / ukupanBrojStudenata * 100;
+                txtProlaznost.Text = $"{Math.Round(prolaznost, 1)}%";
+            }
         }
     }
 }
